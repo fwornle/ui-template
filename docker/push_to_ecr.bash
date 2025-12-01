d354f62ce7d9bcea6cf757d1f10bceb88472d774
fwornle <fwornle@yahoo.co.uk>
2025-12-01 07:19:17 +0100

    fix: revert back to nano-degree template (for coder)

--- docker/push_to_ecr.bash
+++ docker/push_to_ecr.bash
@@ -3,6 +3,6 @@
 set -exuo pipefail
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 799634405166.dkr.ecr.eu-central-1.amazonaws.com
-docker build -t coder/ui-template .
-docker tag coder/ui-template:latest 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/ui-template:latest
-docker push 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/ui-template:latest
+docker build -t coder/agentic-ai-nano .
+docker tag coder/agentic-ai-nano:latest 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/agentic-ai-nano:latest
+docker push 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/agentic-ai-nano:latest
